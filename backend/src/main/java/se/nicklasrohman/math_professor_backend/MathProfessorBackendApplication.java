edc5e46e7e8995b5316fb210dafcb5f3a17f3e86
nicklasrohman <nicklas.rohman@nexergroup.com>
2025-12-01 08:29:27 +0100

    added backend

---
+++ backend/src/main/java/se/nicklasrohman/math_professor_backend/MathProfessorBackendApplication.java
@@ -0,0 +1,13 @@
+package se.nicklasrohman.math_professor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathProfessorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathProfessorBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/nicklasrohman/math_professor_backend/MathProfessorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nicklasrohman.math_professor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathProfessorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
